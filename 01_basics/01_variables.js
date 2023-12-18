1b982fbad881bcd28f875028890b31ee410d0c7f
sachins010 <sachin.shirsath010@gmail.com>
2023-12-19 00:50:46 +0530

    variableJS

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+ const account_Id = 100
+ let account_Email = "sachin123@gmail.com"
+ var account_Password = "Sachin@123"
+ account_city = "Nashik"
+
+ // account_Id = 200 //Assignment to constant variable.
+
+ account_Email = "sss123@gmail.com"
+ account_Password = "sss@123"
+ account_city = "Pune"
+
+ console.log(account_Id);
+
+ console.log([account_Email,account_Password,account_city]);
+ console.table([account_Email,account_Password,account_city]);
+
+ let account_state;// undefined
