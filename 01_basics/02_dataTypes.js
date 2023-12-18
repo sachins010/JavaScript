1da271c998f3b54f4b29ef0fa6652f6a0aaf76c0
sachins010 <sachin.shirsath010@gmail.com>
2023-12-19 00:51:47 +0530

    dataTypeJS

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all js code as newer version
+// now a days we do not need to use "use strict"
+
+/*
+* Data Types
+-number
+-bigint
+-string => ""
+-boolean => true/false
+-null => standalone value " its a representation of empty value & we can assign null"
+-undefined => value is not assigned
+-symbol =>  it use for uniqueness data type
+*/
+
+
+let age = 23 // it's number type
+let name = "Sachin"// it's string type
+let address = null // it's object type
+let married_status = false// it's boolean type
+//let new1 = symbol 
+
+console.log(typeof "Sachin");
