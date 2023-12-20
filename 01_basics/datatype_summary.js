1c33dba6403f395429bff3496c3837cecb37e6b7
sachins010 <sachin.shirsath010@gmail.com>
2023-12-20 14:32:54 +0530

    important explation of dataType

---
+++ 01_basics/datatype_summary.js
@@ -0,0 +1,31 @@
+/*
+    primtive & non primtive 
+
+    1) Primtive
+        -Number
+        -String
+        -boolean
+        -null
+        -undefined
+        -symbol
+        -bigInt
+    
+    2) Non-Primtive (Reference type)
+        -Array
+            let cricketers = ["Sachin","Dhoni","Virat","ROhit"];
+
+        -Object
+            let myObj = {
+            name : "Sachin",
+            age : 22,
+            }
+         in curly bracket means that is object
+
+        -Function
+            const myFunction = function(){
+            console.log("Hello in my js function");
+}
+*/
+
+
+
