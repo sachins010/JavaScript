f18ca8ef460845c914e365894824f6ae66a4c540
sachins010 <sachin.shirsath010@gmail.com>
2023-12-20 14:31:45 +0530

    comparision_operators

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,34 @@
+/*
+   - We know comparision output in boolean form
+   - try to compare the same dataType values 
+   - Normal comparision
+    console.log(2>1);
+    console.log(2>=1);
+    console.log(2<1);
+    console.log(2<=1);
+    console.log(2!=1);
+    console.log(2==1);
+   
+    console.log(2==2); //ture
+    console.log("2"==2); //true
+    console.log("2"===2); //false
+
+    - while comparision dataType must be same
+
+    - comparision with null value
+        console.log(null > 0); // false
+        console.log(null == 0);// false
+        console.log(null >= 0);// true
+    
+    - comparision with undefined value
+        output always false while comparing the value with undefined value
+
+    - strict comparision
+        it checks value with dataType
+        console.log("2"==2); //true
+        console.log("2"===2); //false - its checking dataType as well
+*/
+
+
+
+
